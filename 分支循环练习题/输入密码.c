8a4b35d2302c8826712b2ef0a0d8cd70e0ce96d7
GaoHaiNB <88870728+GaoHaiNB@users.noreply.github.com>
2021-08-14 21:21:59 +0800

    Add files via upload

---
+++ 分支循环练习题/输入密码.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+#include<string.h>
+int main() {
+	//char password[] = "123456";
+	//char mi[20] = { 0 };
+	//int i = 1;
+	//for (i; i < 4; i++)
+	//{
+	//	printf("请输入:>");
+	//	scanf("%s", mi);
+	//	if (strcmp(password, mi) == 0)
+	//	{
+	//		printf("输入正确\n");
+	//		break;
+	//	}
+	//	else
+	//	{
+	//		printf("输入错误\n");
+	//	}
+	//}
+	//if (i > 3)
+	//{
+	//	printf("过5分钟后再试");
+	//}
+	int a;
+	scanf("%d", &a);
+	printf("%d",a);
+
+	return 0;
+}
